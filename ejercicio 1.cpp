a036ddeee6f609c18c28a4fd55e6014cf4bae54c
di3go <djramosm@unjbg.edu.pe>
2024-06-26 17:41:05 -0500

    Se añadió una presentación al programa

--- ejercicio 1.cpp
+++ ejercicio 1.cpp
@@ -3,6 +3,7 @@ using namespace std;
 
 int main () {
 	int num, mult, i = 1;
+	cout << "Programa que imprime los primeros n multiplos de 7" << endl;
 	cout << "Cantidad de multiplos: ";
 	cin >> num;
 	while ( i <= num) {
